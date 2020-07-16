7ce80b3ea22157c0d598c8ef06e8bdc71a4e6a50
Mateusz Holak <holak.mateusz@gmail.com>
2020-07-16 21:52:02 +0200

    Init project

---
+++ app/src/main/java/com/example/mvvmcleanarchitecture/activity/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.mvvmcleanarchitecture.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.mvvmcleanarchitecture.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
